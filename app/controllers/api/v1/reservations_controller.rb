0c1c70905a06025ff74f38a7c6baddf1c7233085
Ashish Singh <aitashish173@gmail.com>
2015-06-27 15:32:05 +0530

    skipped authentication for user for reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReservationsController < ApplicationController
-  #skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
 
   def index
     page = params[:page]
